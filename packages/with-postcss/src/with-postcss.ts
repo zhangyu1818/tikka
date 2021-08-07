712fc7a642f15b26b2279c9d245cf93b23d7d65c
zhangyu1818 <723156735@qq.com>
2021-08-07 16:59:02 +0800

    prettier

--- packages/with-postcss/src/with-postcss.ts
+++ packages/with-postcss/src/with-postcss.ts
@@ -11,26 +11,26 @@ export type WithPostcssOptions = {
 
 const isTransformer = (transform: any): transform is Transform => transform.transformer
 
-const withPostcss: TransformPlugin<WithPostcssOptions> = (transforms, options = {}) => async (
-  state
-) => {
-  const transformsFunc: TransformFunc[] = transforms.map((transform) =>
-    isTransformer(transform) ? transform() : transform
-  )
+const withPostcss: TransformPlugin<WithPostcssOptions> =
+  (transforms, options = {}) =>
+  async (state) => {
+    const transformsFunc: TransformFunc[] = transforms.map((transform) =>
+      isTransformer(transform) ? transform() : transform
+    )
 
-  const { outputFile } = state
+    const { outputFile } = state
 
-  const { plugins, options: processOptions } = options
+    const { plugins, options: processOptions } = options
 
-  const outputWithPostcss: typeof outputFile = async (outputPath, data, joinBase) => {
-    const { css } = await postcss(plugins).process(data, { from: undefined, ...processOptions })
-    await outputFile(outputPath, css, joinBase)
+    const outputWithPostcss: typeof outputFile = async (outputPath, data, joinBase) => {
+      const { css } = await postcss(plugins).process(data, { from: undefined, ...processOptions })
+      await outputFile(outputPath, css, joinBase)
+    }
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const transformFunc of transformsFunc) {
+      await transformFunc({ ...state, outputFile: outputWithPostcss })
+    }
   }
-
-  // eslint-disable-next-line no-restricted-syntax
-  for (const transformFunc of transformsFunc) {
-    await transformFunc({ ...state, outputFile: outputWithPostcss })
-  }
-}
 
 export default withPostcss
