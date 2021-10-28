0f82b679f0be9797687692959d29dc069f605528
zhangyu1818 <723156735@qq.com>
2021-10-29 00:03:11 +0800

    @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
   testEnvironment: 'node',
   setupFiles: ['./jest-setup.js'],
   moduleNameMapper: {
